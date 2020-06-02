7940e42376892342f4b2e6e96e890071f7fcb8f3
Hari.shankar <harishankar.h@gmail.com>
2020-06-02 15:36:41 +0530

    jun20_2

---
+++ src/bejun20/MxSumarrDemo.java
@@ -0,0 +1,37 @@
+package bejun20;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MxSumarrDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		getMaxSum();
+	}
+	
+	public static void getMaxSum()
+	{
+		List<Integer> lst= new ArrayList<Integer>();
+		//int ar[]={-2,-3,4,-1,-2,1,5,-3};
+		int ar[]={0,-4,8,-8, 9,3,7,-3};
+		int mx_sf=0;
+		int mx_ending_here=0;
+		for(int i=0;i<ar.length;i++)
+		{
+			mx_ending_here =mx_ending_here+ ar[i];
+			if(mx_ending_here<0)
+			{
+				mx_ending_here=0;
+			}
+			if(mx_ending_here>mx_sf)
+			{
+				//lst.add(ar[i]);  //not working properly
+				mx_sf=mx_ending_here;
+			}
+		}
+		System.out.println("mx sum : "+ mx_sf);
+		System.out.println(lst);
+	}
+
+}
--- src/reg/RegexDemo.java
+++ src/reg/RegexDemo.java
@@ -6,7 +6,9 @@ public class RegexDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		testPattern();
+		//testPattern1();
+		testFormatter();
+		
 	}
 	
 	public static void testPattern()
@@ -22,5 +24,25 @@ public class RegexDemo {
 		System.out.println("yeeeeeeeeeeeeee");
 		System.out.println(Pattern.matches("[3-4][0-9]{2}", "300"));
 	}
-
+	
+	public static void testPattern1()
+	{
+		System.out.println(Pattern.matches("[a-zA-Z0-9]\2{3}", "aaa"));
+	}
+ public static  void testFormatter()
+ {
+	 int d=14;
+	 String s1= String.format("%7d0", d);
+	 System.out.println(s1);
+	 
+	int a=10;
+	 String name="oonoo";  
+	 String sf1=String.format("name is %s",a);  
+	 String sf2=String.format("value is %f",32.33434);  
+	 String sf3=String.format("value is %32.12f",32.33434);//returns 12 char fractional part filling with 0  
+	   
+	 System.out.println(sf1);  
+	 System.out.println(sf2);  
+	 System.out.println(sf3);  
+ }
 }
